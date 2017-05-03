4fab2f71a04a88230f00f58be2d082e35cfa247b
Sanna Tuovinen <sanna.tuovinen@ntcfin.com>
2017-05-03 14:26:16 +0300

    uusi kenttä

--- models/res_partner.py
+++ models/res_partner.py
@@ -1,7 +1,7 @@
 from openerp import api, fields, models, _
 
 
-class ResPatrner(models.Model)
-_inherit='res.partner'
+class ResPatrner(models.Model):
+		_inherit='res.partner'
 
-is_key_customer = fields.Boolean('Key customer', help='Indicates that the partener is a key customer')
+		is_key_customer = fields.Boolean('Key customer', help='Indicates that the partener is a key customer')
