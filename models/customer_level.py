7de16957bcd15d0f2243f0c02265e86d7bfe0d7f
Sanna Tuovinen <sanna.tuovinen@ntcfin.com>
2017-05-03 15:06:14 +0300

    uusi kenttä

--- models/customer_level.py
+++ models/customer_level.py
@@ -2,7 +2,7 @@ from openerp import api, fields, models, _
 
 
 class CustomerLevel(models.Model):
-		_name='crm_keycustomer.customer_level'
+		_name='crm_key_customer.customer_level'
 		_description = 'Customer Level'
 		
 
