8dc83f1b878b3cd0e8921364a74480e65027fc2a
Sanna Tuovinen <sanna.tuovinen@ntcfin.com>
2017-05-03 15:56:15 +0300

    uusi kenttä

--- models/customer_level.py
+++ models/customer_level.py
@@ -5,7 +5,13 @@ class CustomerLevel(models.Model):
 		_name='crm_key_customer.customer_level'
 		_description = 'Customer Level'
 		
+		def get_partner_count(self):
+			for level in self:
+				number_of_partners = len(level.partner_ids)
+				level.partner_count = number_of_partners
+				
 
 		name = fields.Char('Name', help='e.g. bronze or silver')
 		description = fields.Text('Description')
 		partner_ids = fields.One2many('res.partner','customer_level_id','Customers on this level')
+		partner_count = fields.Integer(compute='get_partner_count', string="Number of customers')
