8e9a218811ab72c93d1dcfc09a304b18b0fd3f7a
Sanna Tuovinen <sanna.tuovinen@ntcfin.com>
2017-05-03 15:32:21 +0300

    uusi kenttä

--- models/res_partner.py
+++ models/res_partner.py
@@ -5,3 +5,4 @@ class ResPatrner(models.Model):
 		_inherit='res.partner'
 
 		is_key_customer = fields.Boolean('Key customer', help='Indicates that the partener is a key customer')
+		customer_level_id = fields.Many2one('crm_key_customer.customer_level', 'Customer level')
